c971b5d90b8df6ab3e85ecc94745be086c5e24ea
Anthony Collini <collini.anthony@gmail.com>
2021-02-15 04:31:40 -0600

    feat: add popup modal when item is added to cart

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,26 +1,33 @@
 import React, { useState } from "react";
 import Description from "../Components/Description";
+import AddCart from "../Components/AddCart";
 
-function Product({ album, addCart, toggleDes }) {
+function Product({ album, addCart }) {
   const [showDes, setShowDes] = useState(false);
-
+  const [showAdd, setShowAdd] = useState(false);
   const hanldeClick = () => {
     addCart(album);
-    alert(`${album.title} has been added to cart`);
+    setShowAdd(true);
+    setTimeout(() => {
+      setShowAdd(false);
+    }, 4000);
   };
 
   const handleImgClick = () => {
     setShowDes(true);
+    document.addEventListener("mousedown", closeDes);
   };
 
   const closeDes = () => {
     setShowDes(false);
+    document.removeEventListener("mousedown", closeDes);
   };
 
   return (
     <div>
       <h2>{album.title}</h2>
-      <Description close={closeDes} show={showDes} album={album} />
+      {showDes ? <Description album={album} /> : null}
+      {showAdd ? <AddCart album={album} /> : null}
       <img
         className="albumCover"
         src={album.image}
