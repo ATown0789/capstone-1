5c50fcebb84abc2dd42effca64deb7a531ebc979
Anthony Collini <collini.anthony@gmail.com>
2021-02-16 13:55:54 -0600

    bug fix: nav menu only close on 'x' click

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as CartImage } from "../images/shopping_cart-24px.svg";
 import NumItems from "./NumItems";
 
 function NavBar({ cartNum }) {
+  const navLabel = useRef();
+
+  const handleClick = () => {
+    document.addEventListener("mousedown", uncheck);
+  };
+
+  const uncheck = () => {
+    if (navLabel.current.checked) navLabel.current.checked = false;
+
+    document.removeEventListener("mosedown", uncheck);
+  };
   return (
     <div>
       <Link to="/shopping-cart">
@@ -11,16 +22,11 @@ function NavBar({ cartNum }) {
         <CartImage id="cartImage" fill="currentColor" viewBox="10 0 5 30" />
       </Link>
       <nav>
-        <input type="checkbox" id="navCheck" />
-        <label id="navBtn" for="navCheck">
+        <input ref={navLabel} type="checkbox" id="navCheck" />
+        <label id="navBtn" for="navCheck" onClick={handleClick}>
           <div className="menuBar"></div>
         </label>
         <ul id="navMenu">
-          <li>
-            <Link className="menuItem" to="/">
-              Welcome
-            </Link>
-          </li>
           <li>
             <Link className="menuItem" to="/about">
               About
